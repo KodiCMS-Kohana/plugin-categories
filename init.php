8ef82df286f8f32b615d9569b4b9f11606a29b4d
Butschster <butschster@gmail.com>
2015-02-01 16:47:10 +0300

    Смена версии и добавлена минимально необходимая версия KodiCMS

--- init.php
+++ init.php
@@ -2,4 +2,7 @@
 
 Plugin::factory('categories', array(
 	'title' => 'Datasource Categories',
+	'version' => '1.0.5',
+	'author' => 'KodiCMS',
+	'required_cms_version' => '13.82.135'
 ))->register();
